639ac5fee3be7192be5565fae7da9aebb6872815
rizz-cloud <rh05@hotmail.co.uk>
2025-08-29 21:50:38 +0100

    updated code for next js

---
+++ portfolio-infrastructure/lib/portfolio-infrastructure-stack.ts
@@ -0,0 +1,59 @@
+import * as cdk from 'aws-cdk-lib';
+import * as amplify from '@aws-cdk/aws-amplify-alpha';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class PortfolioInfrastructureStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Amplify Application
+    const amplifyApp = new amplify.App(this, 'PortfolioApplication', {
+      appName: 'Portfolio',
+      // Connect to my github repo
+      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+        owner: 'CloudRizz',
+        repository: 'portfolio_website',
+        oauthToken: cdk.SecretValue.secretsManager('github-token') 
+      }),
+
+      // Build Specification
+      buildSpec: codebuild.BuildSpec.fromObjectToYaml({
+        version: '1.0',
+        frontend: {
+          phases: {
+            preBuild: {
+              commands: [
+                'echo "Starting this build"', 
+                'cd portfolio',
+                'npm install'
+              ],
+            },
+            build: {
+              commands: [
+                'echo "Building our NextJs app"',
+                'npm run build-and-export',
+                'echo "Build is completed"', 
+              ],
+            },
+          },
+          artifacts: {
+            baseDirectory: 'portfolio/out',
+            files: ['**/*']
+          },
+          cache: { 
+            paths: [
+              'node/modules/**/*',
+              '.next/cache/**/*',
+              ],
+            }, 
+          },
+        }),
+      });
+
+    const mainBranch = amplifyApp.addBranch('main',{
+      autoBuild: true
+    });
+  }
+}
